8f4c8a2e4a27215f5dcce0b5fb0456a2329c904d
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2021-07-04 12:17:39 -0400

    Receive jotform as form-urlencoded

--- controllers/jotform.controller.server.js
+++ controllers/jotform.controller.server.js
@@ -2,24 +2,19 @@ import { processImage } from '../services/image.service.server.js';
 import formidable from 'formidable';
 
 export default (app) => {
-
     app.post('/api/jotform', async (req, res) => {
         try {
-            const form = formidable({ multiples: true, uploadDir: './tmp' });
+            console.log(req);
+            res.status(500).send('Test!');
 
-            form.parse(req, async (err, fields, files) => {
-                //fields.submission_id
-                console.log({fields});
-                console.log({files});
-                // const img = await processImage(files.source);
-                // res.contentType('image/jpeg');
-                // const data = await img.arrayBuffer();
-                // res.send(Buffer.from(data));
-                res.status(500).send('Test!')
-            });
+            // const img = await processImage(files.source);
+            // res.contentType('image/jpeg');
+            // const data = await img.arrayBuffer();
+            // res.send(Buffer.from(data));
+            
         } catch (error) {
             console.error(error);
-            res.status(500).send('Error!')
+            res.status(500).send('Error!');
         }
     });
 };
