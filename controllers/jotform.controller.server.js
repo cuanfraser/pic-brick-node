5823efd8e33af2f9c05a91eb2b158c873c50c68c
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2021-07-04 13:11:47 -0400

    Accept form correctly, attempt getting image source

--- controllers/jotform.controller.server.js
+++ controllers/jotform.controller.server.js
@@ -1,19 +1,29 @@
 import { processImage } from '../services/image.service.server.js';
-import formidable from 'formidable';
+import { get } from 'lodash-es';
+import { getSourceImage } from '../services/jotform.service.server.js';
 
 export default (app) => {
     app.post('/api/jotform', async (req, res) => {
         try {
             console.log(req);
+
+            const formId = req.body.formID;
+            const subId = req.body.submission_id;
+            for (const fileName of req.body['fileupload[]']) {
+                const image = await getSourceImage(formId, subId, fileName);
+                console.log(image);
+            }
+
             res.status(500).send('Test!');
+
 
             // const img = await processImage(files.source);
             // res.contentType('image/jpeg');
             // const data = await img.arrayBuffer();
             // res.send(Buffer.from(data));
-            
         } catch (error) {
             console.error(error);
+            console.error(error.stack);
             res.status(500).send('Error!');
         }
     });
