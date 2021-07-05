08d876f150b5231738e39aa3088d90ffab512e7b
Cuan <33520989+cuanfraser@users.noreply.github.com>
2021-07-05 16:46:50 -0400

    Move cartoon API url to constants

--- src/services/image.service.server.js
+++ src/services/image.service.server.js
@@ -1,13 +1,12 @@
 import fetch from 'node-fetch';
 import FormData from 'form-data';
-
-const API_URL = 'https://master-white-box-cartoonization-psi1104.endpoint.ainize.ai/predict';
+import { CARTOON_API_URL } from '../constants';
 
 const cartoonifyImage = async (img) => {
     const formData = new FormData();
     formData.append('file_type', 'image');
     formData.append('source', img, 'input image.jpg');
-    return fetch(API_URL, {
+    return fetch(CARTOON_API_URL, {
         method: 'POST',
         body: formData,
     });
