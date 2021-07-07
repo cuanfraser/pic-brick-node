eb9f34e594f844a640139ef67ab0d991f01a1823
Cuan <33520989+cuanfraser@users.noreply.github.com>
2021-07-07 00:00:46 -0400

    Added env variables for API key and username

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
-export const JOTFORM_USERNAME = 'glebbulatovskiy';
+export const JOTFORM_USERNAME = process.env.JOTFORM_USERNAME;
 export const JOTFORM_UPLOAD_URL = 'https://www.jotform.com/uploads';
-export const JOTFORM_API_KEY = '';
+export const JOTFORM_API_KEY = process.env.JOTFORM_API_KEY;
 
 export const CARTOON_API_URL =
     'https://master-white-box-cartoonization-psi1104.endpoint.ainize.ai/predict';
