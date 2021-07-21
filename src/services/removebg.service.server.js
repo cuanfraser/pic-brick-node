2c62414372f16c5f1966d13730c91c4051707942
Cuan <33520989+cuanfraser@users.noreply.github.com>
2021-07-21 19:53:28 -0400

    Started removeBg

---
+++ src/services/removebg.service.server.js
@@ -0,0 +1,31 @@
+import { REMOVE_BG_API_KEY, REMOVE_BG_URL } from '../constants.js';
+import fetch from 'node-fetch';
+import FormData from 'form-data';
+
+const removeBackground = async (img) => {
+    console.time('removeBg');
+
+    const formData = new FormData();
+    // TODO: Change to Auto on production
+    formData.append('size', 'preview');
+    formData.append('image_file', img);
+    formData.append('type', 'auto');
+    formData.append('format', 'jpg');
+    formData.append('bg_color', '#7FCAE0');
+
+    const resp = await fetch(REMOVE_BG_URL, {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'X-Api-Key': REMOVE_BG_API_KEY,
+        },
+    });
+
+    // TODO: Error Checking and Catch
+    if (resp.statusCode != 200) {
+        console.error('Error on removeBg: ' + resp.statusCode);
+    }
+
+    console.timeEnd('removeBg');
+    return resp;
+};
