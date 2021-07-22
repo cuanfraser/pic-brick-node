32a7979f20f9181cb4e36c68a4a9a080a3ba9634
Cuan <33520989+cuanfraser@users.noreply.github.com>
2021-07-21 20:58:35 -0400

    Load brick imgs with IIFE

--- src/services/file.service.server.js
+++ src/services/file.service.server.js
@@ -2,11 +2,14 @@ import Canvas from 'canvas';
 import { HEX_COLOUR_PALETTE } from '../constants.js';
 
 const brickImgs = {};
-for (const hex of HEX_COLOUR_PALETTE) {
-    const name = hex.substring(1);
-    const blockImgPath = new URL(`../../resources/bricks/${name}_32x32.png`, import.meta.url);
-    const blockImg = await Canvas.loadImage(blockImgPath.pathname);
-    brickImgs[name] = blockImg;
-}
+// Load images of individual bricks
+(async () => {
+    for (const hex of HEX_COLOUR_PALETTE) {
+        const name = hex.substring(1);
+        const blockImgPath = new URL(`../../resources/bricks/${name}_32x32.png`, import.meta.url);
+        const blockImg = await Canvas.loadImage(blockImgPath.pathname);
+        brickImgs[name] = blockImg;
+    }
+})();
 
 export { brickImgs };
