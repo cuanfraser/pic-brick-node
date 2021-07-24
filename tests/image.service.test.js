9d102790a5a62b041af28247b177a8f7b98ab59b
Cuan <33520989+cuanfraser@users.noreply.github.com>
2021-07-24 19:03:10 -0400

    Revert "Added Email testing, Instructions, Count of colours"

--- tests/image.service.test.js
+++ tests/image.service.test.js
@@ -1,3 +1,4 @@
+import fetch from 'node-fetch';
 import {
     closestColourInPalette,
     cropImageToBoardSize,
@@ -6,7 +7,7 @@ import {
 describe('closestColourInPalette Tests', () => {
     test('Chooses correct colour if given exact match', () => {
         const output = closestColourInPalette(215, 135, 57, 1);
-        expect(output).toEqual('#D78739');
+        expect(output).toEqual([215, 135, 57]);
     });
 });
 
@@ -37,21 +38,3 @@ describe('cropImageToBoardSize Tests', () => {
         }
     });
 });
-
-// describe('makeImageBricks Tests', () => {
-//     test('Correct number of bricks used', () => {
-//         const output = cropImageToBoardSize(96, 96, 480, 480);
-//         expect(output).toEqual({ newWidth: 480, newHeight: 480, widthCrop: 0, heightCrop: 0 });
-//     });
-
-//     test('Various sizes, factor of square board size', () => {
-//         const boardSize = 96;
-//         for (let i = 0; i < 10; i++) {
-//             const width = Math.floor(Math.random() * 1000);
-//             const height = Math.floor(Math.random() * 1000);
-//             const output = cropImageToBoardSize(boardSize, boardSize, width, height);
-//             expect(output.newWidth % boardSize).toBe(0);
-//             expect(output.newHeight % boardSize).toBe(0);
-//         }
-//     });
-// });
