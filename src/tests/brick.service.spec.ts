d6e1fe12bb9106291007b20f8fc8ec3265fa076d
Cuan <cuanwfraser@gmail.com>
2022-05-12 00:10:25 +0000

    fix test

--- src/tests/brick.service.spec.ts
+++ src/tests/brick.service.spec.ts
@@ -1,8 +1,9 @@
+import { HEX_COLOUR_PALETTE } from '../constants';
 import { closestColourInPalette } from '../services/brick.service';
 
 describe('closestColourInPalette Tests', () => {
     test('Chooses correct colour if given exact match', () => {
-        const output = closestColourInPalette(215, 135, 57);
+        const output = closestColourInPalette(215, 135, 57, HEX_COLOUR_PALETTE);
         expect(output).toEqual('#D78739');
     });
 });
