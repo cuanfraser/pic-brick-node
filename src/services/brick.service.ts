acd41bec854fdf8a204abc0c879f8d730694ef65
Cuan <cuanwfraser@gmail.com>
2022-05-12 00:06:24 +0000

    only include hex with more than 5

--- src/services/brick.service.ts
+++ src/services/brick.service.ts
@@ -16,8 +16,8 @@ const brickImgs: { [key: string]: Canvas.Image; } = {};
 })();
 
 // Finds closest hex colour in colour palette when given RGB val
-const closestColourInPalette = (r: number, g: number, b: number): string => {
-    const matcher = nearestColour.from(HEX_COLOUR_PALETTE);
+const closestColourInPalette = (r: number, g: number, b: number, palette: string[]): string => {
+    const matcher = nearestColour.from(palette);
     return matcher(`rgb(${r}, ${g}, ${b})`);
 };
 
@@ -90,21 +90,49 @@ const pixelateImage = async (src: Buffer, widthBlocks: number, heightBlocks: num
             const bAvg = Math.floor(bVals.reduce((acc, cur) => acc + cur) / bVals.length);
 
             // Find closest RGB colour in palette
-            const match = closestColourInPalette(rAvg, gAvg, bAvg);
+            const match = closestColourInPalette(rAvg, gAvg, bAvg, HEX_COLOUR_PALETTE);
             const count = hexToCount.get(match);
             hexToCount.set(match, count ? count + 1 : 1);
-
-            brickImageCtx.drawImage(brickImgs[match], (x / sampleSize) * 32, (y / sampleSize) * 32);
-            // brickImageCtx.fillStyle = match;
-            // brickImageCtx.fillRect((x / sampleSize) * 32, (y / sampleSize) * 32, 32, 32);
         }
     }
 
-    hexToCount.forEach((value: number, key: string) => {
-        console.groupCollapsed("hex use");
-        console.log(key, value);
-        console.groupEnd();
+    const newPalette = HEX_COLOUR_PALETTE.filter((hex) => {
+        let count = hexToCount.get(hex);
+        if (count == undefined) {
+            count = 0;
+        }
+        return count > 5;
     });
+
+    for (let y = 0; y < newHeight; y += sampleSize) {
+        for (let x = 0; x < newWidth; x += sampleSize) {
+            const p = (x + y * newWidth) * 4;
+
+            // Average RGB vals over sample size
+            //   Collect RGB values over sample size
+            const rVals = [];
+            const gVals = [];
+            const bVals = [];
+            for (let i = p; i < p + sampleSize * 4; i += 4) {
+                const r = pixelArr[i];
+                const g = pixelArr[i + 1];
+                const b = pixelArr[i + 2];
+
+                rVals.push(r);
+                gVals.push(g);
+                bVals.push(b);
+            }
+            //   Average RGB vals
+            const rAvg = Math.floor(rVals.reduce((acc, cur) => acc + cur) / rVals.length);
+            const gAvg = Math.floor(gVals.reduce((acc, cur) => acc + cur) / gVals.length);
+            const bAvg = Math.floor(bVals.reduce((acc, cur) => acc + cur) / bVals.length);
+
+            // Find closest RGB colour in palette
+            const match = closestColourInPalette(rAvg, gAvg, bAvg, newPalette);
+
+            brickImageCtx.drawImage(brickImgs[match], (x / sampleSize) * 32, (y / sampleSize) * 32);
+        }
+    }
 
     const output = brickImageCan.toBuffer('image/jpeg', { quality: 0.75 });
 
