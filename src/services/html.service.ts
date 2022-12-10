909e1661a68513fafeed35a8bdd1d45a8bd96e78
Cuan <cuanwfraser@gmail.com>
2022-12-10 16:27:29 -0500

    remove temp html code

--- src/services/html.service.ts
+++ src/services/html.service.ts
@@ -1,12 +1,12 @@
-import fs from "fs/promises"
+// import fs from "fs/promises"
 
-const saveBufferToFile = async (
-    imageBuffer: Buffer
-): Promise<string> => {
-    const fileUrl = new URL(`../../resources/submissions/test.jpeg`, import.meta.url);
-    const file = await fs.writeFile(fileUrl, imageBuffer)
+// const  saveBufferToFile = async (
+//     imageBuffer: Buffer
+// ): Promise<string> => {
+//     const fileUrl = new URL(`../../resources/submissions/test.jpeg`, import.meta.url);
+//     const file = await fs.writeFile(fileUrl, imageBuffer)
 
-    return fileUrl.toString();
-}
+//     return fileUrl.toString();
+// }
 
-export { saveBufferToFile };
+// export { saveBufferToFile };
