9266a8970dc3c068660cc6be53662758534cb8e1
Cuan <cuanwfraser@gmail.com>
2023-01-09 23:45:58 +0400

    Remove tests

--- src/tests/brick.service.spec.ts
+++
@@ -1,9 +0,0 @@
-import { HEX_COLOUR_PALETTE } from '../constants';
-import { closestColourInPalette } from '../services/brick.service';
-
-describe('closestColourInPalette Tests', () => {
-    test('Chooses correct colour if given exact match', () => {
-        const output = closestColourInPalette(215, 135, 57, HEX_COLOUR_PALETTE);
-        expect(output).toEqual('#D78739');
-    });
-});
