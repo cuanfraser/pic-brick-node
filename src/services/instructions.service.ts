af33e249fc27380a6b56825a0b0ea1d5d0ab7659
cuanfraser <33520989+cuanfraser@users.noreply.github.com>
2023-03-25 20:35:27 -0400

    Formatting

--- src/services/instructions.service.ts
+++ src/services/instructions.service.ts
@@ -1,23 +1,25 @@
-import { HEX_COLOUR_NUM_MAP } from "../constants.js";
-import { JotformSubmissionModel } from "../models/jotform-submission/jotform-submission.model.js";
-import { IMosaic } from "../models/mosaic/mosaic.schema.js";
+import { HEX_COLOUR_NUM_MAP } from '../constants.js';
+import { JotformSubmissionModel } from '../models/jotform-submission/jotform-submission.model.js';
+import { IMosaic } from '../models/mosaic/mosaic.schema.js';
 
+export const getInstructionsForSubmission = async (id: string): Promise<string> =>
+    JotformSubmissionModel.findOne({ submissionId: id })
+        .populate<{ mosaics: [IMosaic] }>('mosaics')
+        .orFail()
+        .then((sub) => {
+            let csvString = '';
 
-export const getInstructionsForSubmission = async (id: string): Promise<string> => 
-    JotformSubmissionModel.findOne({submissionId: id}).populate<{mosaics: [IMosaic]}>('mosaics').orFail().then(sub=> {
-        let csvString = '';
+            sub.mosaics.forEach((mosaic) => {
+                let currentString = `${mosaic.originalImageName}(${mosaic.size})\n`;
+                mosaic.instructions.forEach((row) => {
+                    row.forEach((column) => {
+                        const id = HEX_COLOUR_NUM_MAP.get(column);
+                        currentString += id + ',';
+                    });
+                    currentString += '\n';
+                });
+                csvString += currentString + '\n';
+            });
 
-        sub.mosaics.forEach(mosaic => {
-            let currentString = `${mosaic.originalImageName}(${mosaic.size})\n`;
-            mosaic.instructions.forEach(row => {
-                row.forEach(column => {
-                    const id = HEX_COLOUR_NUM_MAP.get(column)
-                    currentString += id + ',';
-                })
-                currentString += '\n';
-            })
-            csvString += currentString + '\n';
-        })
-
-        return csvString;
-    });
+            return csvString;
+        });
